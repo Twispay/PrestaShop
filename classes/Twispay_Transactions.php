9df08b6e13c02dc0e9bab1d17cb7f3666524ffee
Michael Rotaru <rightsidedigital@gmail.com>
2019-12-06 12:24:20 +0200

    Final check modifications

--- classes/Twispay_Transactions.php
+++ classes/Twispay_Transactions.php
@@ -81,7 +81,7 @@ if (! class_exists('Twispay_Transactions')) :
             }
             /** Keep just the customer id from identifier */
             if (!empty($data['identifier']) && strpos($data['identifier'], '_') !== false) {
-               $explodedVal = explode("_", $data['identifier'])[1];
+               $explodedVal = explode("_", $data['identifier'])[2];
                /** Check if customer id contains only digits and is not empty */
                if(!empty($explodedVal) && ctype_digit($explodedVal)){
             	   $data['identifier'] = pSQL($explodedVal);
